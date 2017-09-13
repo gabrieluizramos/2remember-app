6b5c670e8e392b7e72961e56b2a159630f60ecc7
Gabriel Ramos <gabriel.l.ramos@walmart.com>
2017-09-13 10:25:21 -0300

    Bugfix at verificarCompromissos

--- assets/js/controllers/mainCtrl.js
+++ assets/js/controllers/mainCtrl.js
@@ -1,62 +1,64 @@
 // referencia
-angular.module( '2remember' ).controller( 'mainCtrl' , function( $scope , lembretesAPI ){
-    
+angular.module('2remember').controller('mainCtrl', function ($scope, lembretesAPI) {
+
     $scope.lembretesNotificados = [];
     $scope.urlList = window.urlList;
-    
-    $scope.permitirNotificacoes = (function(){
-        if( Notification.persmission != 'granted' ){
-            Notification.requestPermission( function( result ) {
-        	  if ( result != 'granted' ) {
-        	      alert( 'Não será permitido utilizar o app corretamente sem a permissão de notificações.' )
-        	  }
-        	});
+
+    $scope.permitirNotificacoes = (function () {
+        if (Notification.persmission != 'granted') {
+            Notification.requestPermission(function (result) {
+                if (result != 'granted') {
+                    alert('Não será permitido utilizar o app corretamente sem a permissão de notificações.')
+                }
+            });
         }
     })();
-    
-    $scope.enviarNotificacao = function( conteudoMsg ){
-        
-        navigator.serviceWorker.ready.then( function( registration ) {
-            registration.showNotification( '2Remember' , conteudoMsg );
-        });
-    	
-    }
-    
-    $scope.verificarCompromissos = (function(){
-        
-        navigator.serviceWorker.register( $scope.urlList.defaultUrl + 'assets/js/workers/sw.js' );
-        
-        window.setInterval( function(){
-            
-            var lembretes = lembretesAPI.retornarLembretes();
-            var qtdLembretes = lembretes.length;
-            
-            for ( var i = 0 ; i < qtdLembretes ; i++ ){
-                
-                var idLembrete = parseInt( lembretes[ i ].id );
-                var horaLembrete = parseInt( lembretes[ i ].dados.hora );
-                var minutoLembrete = parseInt( lembretes[ i ].dados.minuto );
-                var antecedencia = parseInt( lembretes[ i ].dados.antecedencia );
-                var nomeLembrete = lembretes[ i ].dados.nome;
-                
-                var agora = new Date();
 
-                if( horaLembrete == agora.getHours() && ( ( agora.getMinutes() + antecedencia ) == minutoLembrete ) && ( $scope.lembretesNotificados.indexOf( idLembrete ) < 0 ) ){
-                    
-                    var conteudoMsg = {
-                        body : 'Lembre-se do seu compromisso "'+ nomeLembrete +'". Ele começará em ' + antecedencia + ' minutos!' ,
-                        icon : $scope.urlList.defaultUrl + 'assets/img/icon.png'
+    $scope.enviarNotificacao = function (conteudoMsg) {
+
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.showNotification('2Remember', conteudoMsg);
+        });
+
+    }
+
+    $scope.verificarCompromissos = (function () {
+
+        navigator.serviceWorker.register($scope.urlList.defaultUrl + 'assets/js/workers/sw.js');
+
+        window.setInterval(function () {
+
+            var lembretes = lembretesAPI.retornarLembretes();
+
+            if (lembretes && lembretes.length) {
+                var qtdLembretes = lembretes.length;
+
+                for (var i = 0; i < qtdLembretes; i++) {
+
+                    var idLembrete = parseInt(lembretes[i].id);
+                    var horaLembrete = parseInt(lembretes[i].dados.hora);
+                    var minutoLembrete = parseInt(lembretes[i].dados.minuto);
+                    var antecedencia = parseInt(lembretes[i].dados.antecedencia);
+                    var nomeLembrete = lembretes[i].dados.nome;
+
+                    var agora = new Date();
+
+                    if (horaLembrete == agora.getHours() && ((agora.getMinutes() + antecedencia) == minutoLembrete) && ($scope.lembretesNotificados.indexOf(idLembrete) < 0)) {
+
+                        var conteudoMsg = {
+                            body: 'Lembre-se do seu compromisso "' + nomeLembrete + '". Ele começará em ' + antecedencia + ' minutos!',
+                            icon: $scope.urlList.defaultUrl + 'assets/img/icon.png'
+                        }
+
+                        $scope.enviarNotificacao(conteudoMsg);
+                        $scope.lembretesNotificados.push(idLembrete);
+
                     }
-                    
-                    $scope.enviarNotificacao( conteudoMsg );
-                    $scope.lembretesNotificados.push( idLembrete );
-                    
                 }
-                
             }
-            
-        } , 1000 );
-        
+
+        }, 1000);
+
     })();
-    
+
 });
